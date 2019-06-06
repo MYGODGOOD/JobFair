8cfa2e8068cf023b644b440d56944beb3b52ebaf
root <2313583800@qq.com>
2019-06-06 10:02:14 +0800

    t-commit

--- src/main/java/com/zp/controller/TestController.java
+++ src/main/java/com/zp/controller/TestController.java
@@ -1,4 +1,7 @@
 package com.zp.controller;
 
 public class TestController {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
 }
--- src/main/java/com/zp/service/impl/TestService.java
+++ src/main/java/com/zp/service/impl/TestService.java
@@ -1,4 +1,5 @@
 package com.zp.service.impl;
 
 public class TestService {
+
 }
