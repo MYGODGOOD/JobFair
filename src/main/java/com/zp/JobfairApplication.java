900e832117b4632ef0e1767402aeee79e0491928
lxj.cy <lxj.cy@foxmail.com>
2019-06-20 19:29:01 +0800

    lxj's interface

--- src/main/java/com/zp/JobfairApplication.java
+++ src/main/java/com/zp/JobfairApplication.java
@@ -1,5 +1,6 @@
 package com.zp;
 
+import com.sun.media.jfxmedia.logging.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
